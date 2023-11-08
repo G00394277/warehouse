608aa615831138848f35c2c3a24a5219a181b30f
G00394277 <G00394277@atu.ie>
2023-11-08 15:20:49 +0000

    Initial commit

---
+++ src/main/java/ie/atu/warehouse/WarehouseApplication.java
@@ -0,0 +1,15 @@
+package ie.atu.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class WarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
